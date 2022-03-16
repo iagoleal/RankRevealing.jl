2ccdf7f9effaa627247405adf5f7bac9c3d02868
Iago Leal <iago.lealf@gmail.com>
2022-03-16 19:31:26 -0300

    (test) Use exact components whenever possible

--- test/lu.jl
+++ test/lu.jl
@@ -6,6 +6,8 @@ using LinearAlgebra
 # Takes a PLUQ object into the original matrix
 recomp(x) = ([x.L ; x.M] * [x.U x.V])[x.p, x.q]
 
+exactrand(n...) = big.(rand(Int64, n...) .// rand(Int64, n...))
+
 # Check that the decomposition
 # can be multiplied into the right matrix
 function correctness(A::AbstractArray{T}) where {K <: AbstractFloat, S <: Complex{K}, T <: Union{S, K}}
@@ -19,14 +21,14 @@ correctness(A) = recomp(pluq(A)) == A
   @test correctness(zeros(0,1))
   @test correctness(zeros(1,0))
   # Non-degenerate zero matrix
-  @test correctness(zeros(1,1))   # Scalar
+  @test correctness(zeros(1,1))   # Scalar zero
   @test correctness(zeros(1,2))   # Column zero vector
   @test correctness(zeros(2, 1))  # Row zero vector
   # Non-zero
   @test correctness([3;;])        # Scalar
   @test correctness([1;2;;])      # Column vector
   @test correctness([0;2;;])      # column with pivoting
-  @test correctness([1 2;])       # Row column vector
+  @test correctness([1 2;])       # Row vector
   @test correctness([0 2;])       # row with pivoting
 end
 
@@ -41,7 +43,7 @@ end
   @test correctness(A)
 end
 
-@testset "Rectangular matrices: ($m by $m)" for m in 0:9, n in 0:9
+@testset "Random matrices: ($m by $n)" for m in 0:9, n in 0:9
   @testset "Floating point components" begin
     @test correctness(rand(m,n))   # Uniform components
     @test correctness(randn(m,n))  # Gaussian matrix
@@ -49,14 +51,15 @@ end
 
   @testset "Exact Rational components" begin
     # We want to use BigInt and Rational to ensure exact arithmetic
-    A = big.(rand(Int64, m, n) .// rand(Int64, m, n))
+    A = exactrand(m, n)
     @test correctness(A)
   end
 
   @testset "Complex components" begin
     @test correctness(rand(Complex{Float64}, m, n))
-    A = big.(rand(Int64, m, n) .// rand(Int64, m, n))
-    B = big.(rand(Int64, m, n) .// rand(Int64, m, n))
+    # Complex with exact rational components
+    A = exactrand(m, n)
+    B = exactrand(m, n)
     @test correctness(A + im*B)
     # Unit complex matrices
     A = rand(Complex{Float64}, m, n)
@@ -66,11 +69,20 @@ end
 end
 
 @testset "Destructuring" begin
-  A = rand(5, 7)
+  A = exactrand(5, 7)
   PLUQ = pluq(A)
   p, L, U, V, M, q = PLUQ
   # Using permutation vectors
-  @test ([L ; M] * [U V])[p, q] ≈ A atol=1e-10
+  @test ([L ; M] * [U V])[p, q] == A
   # Using permutation matrices
-  @test (PLUQ.P * [L ; M] * [U V] * PLUQ.Q) ≈ A atol=1e-10
+  @test (PLUQ.P * [L ; M] * [U V] * PLUQ.Q) == A
+end
+
+@testset "Uniqueness of LU" begin
+  A = exactrand(5, 5)
+  L = tril(A, -1) + I   # Unit lower trianguler
+  U = triu(A)           # Upper triangular
+  C = L * U
+  F = pluq(C)
+  @test C == F.L * F.U
 end
